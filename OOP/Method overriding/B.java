310fc4e04cb35ac64e6232b413bf1a6d0952da33
Sanjith1698 <156409151+Sanjith1698@users.noreply.github.com>
2025-09-25 10:57:24 +0530

    Add method overriding example in Java
    Introduced classes A and B to demonstrate method overriding, with B extending A and overriding the getX() method. Added Main.java to instantiate B and call the overridden and subclass methods.

---
+++ OOP/Method overriding/B.java
@@ -0,0 +1,15 @@
+class B extends A{
+	int y;
+	
+	B(){
+		this.y = 70;
+	}
+	
+	void getY(){
+		System.out.println("BY : "+y);
+	}
+	
+	void getX(){
+		System.out.println("BX : "+x);
+	}
+}
---
+++ OOP/Method overriding/Main.java
@@ -0,0 +1,9 @@
+class Main{
+	public static void main(String args[]){
+		B obj1 = new B();
+		
+		obj1.getX();
+		obj1.getY();
+	}
+
+}
